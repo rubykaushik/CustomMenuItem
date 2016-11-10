27ea29f03506401291e6f2aa982a95e22a530e0f
Ruby Kaushikuser.email <kaushikruby0@gmail.com>
2016-11-10 09:54:23 +0530

    Initial Commit

---
+++ VisualEffectView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VisualEffectView
+//
+//  Created by Ruby Kaushik on 10/11/16.
+//  Copyright © 2016 Ruby Kaushik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ VisualEffectView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VisualEffectView
+//
+//  Created by Ruby Kaushik on 10/11/16.
+//  Copyright © 2016 Ruby Kaushik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
